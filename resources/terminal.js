558093011903836cdc9769f73ac1bb6674fadeb8
Wojciech Frącz <fraczwojciech@gmail.com>
2017-04-18 00:15:17 +0200

    Ready up to babel

--- resources/terminal.js
+++ resources/terminal.js
@@ -1,10 +1,12 @@
 Reveal.addEventListener('fragmentshown', function (e) {
-    var command = $(e.fragment).attr('run-command');
-    if (command) {
+    var runCommand = $(e.fragment).attr('run-command');
+    if (runCommand !== undefined) {
         var terminal = $(e.fragment).parent('section').find('.terminal:not(.executed)');
         if (terminal.length) {
+            var command = terminal.find('.command .text').text().trim();
+            var commandTime = terminal.attr('time') || 2000;
             terminal.find('.command').typed({
-                strings: ["npm init -y"],
+                strings: [command],
                 typeSpeed: 80,
                 callback: function () {
                     setTimeout(function () {
@@ -12,8 +14,8 @@ Reveal.addEventListener('fragmentshown', function (e) {
                         setTimeout(function () {
                             terminal.addClass('executed');
                             terminal.removeClass('executing');
-                            terminal.find('.result').html($(e.fragment).html().trim());
-                        }, 2000);
+                            // terminal.find('.result').html($(e.fragment).html().trim());
+                        }, commandTime);
                     }, 1500);
                 }
             });
