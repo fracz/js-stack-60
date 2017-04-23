c139b12795bd514f3a545ee9e3ef12d411984eb4
Wojciech Frącz <fraczwojciech@gmail.com>
2017-04-23 23:14:01 +0200

    Till webpack

--- resources/terminal.js
+++ resources/terminal.js
@@ -5,18 +5,22 @@ Reveal.addEventListener('fragmentshown', function (e) {
         if (terminal.length) {
             var command = terminal.find('.command .text').text().trim();
             var commandTime = terminal.attr('time') || 2000;
+            var commandDelay = terminal.attr('delay') || 1500;
+            var typeSpeed = +terminal.attr('speed') || 80;
             terminal.find('.command').typed({
                 strings: [command],
-                typeSpeed: 80,
+                typeSpeed: typeSpeed,
                 callback: function () {
                     setTimeout(function () {
-                        terminal.addClass('executing');
+                        if (commandTime > 0) {
+                            terminal.addClass('executing');
+                        }
                         setTimeout(function () {
                             terminal.addClass('executed');
                             terminal.removeClass('executing');
                             // terminal.find('.result').html($(e.fragment).html().trim());
                         }, commandTime);
-                    }, 1500);
+                    }, commandDelay);
                 }
             });
         }
