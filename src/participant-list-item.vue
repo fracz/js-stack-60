041d59d0238429cbbb613c2c6f2803d8a281220c
Wojciech Frącz <fraczwojciech@gmail.com>
2017-05-09 21:29:25 +0200

    Step 7: Removing participant

---
+++ src/participant-list-item.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="item">
+    <h5>{{ participant }}</h5>
+    <button @click="remove">Usuń</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['participant'],
+    methods: {
+      remove() {
+        this.$emit('remove', this.participant);
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  h5 {
+    margin: 0;
+    font-size: 1em;
+  }
+
+  .item {
+    margin-bottom: 15px;
+  }
+</style>
--- src/participants-list.vue
+++ src/participants-list.vue
@@ -2,15 +2,26 @@
   <div>
     <h1>Zapisane osoby ({{ participants.length }})</h1>
     <ol>
-      <li v-for="participant in participants">{{ participant }}</li>
+      <li v-for="participant in participants">
+        <participant-list-item :participant="participant" @remove="removeParticipant"></participant-list-item>
+      </li>
     </ol>
     <div class="empty" v-if="participants.length == 0">Brak zapisanych osób</div>
   </div>
 </template>
 
 <script>
+  import ParticipantListItem from "./participant-list-item.vue";
+
   export default {
-    props: ['participants']
+    props: ['participants'],
+    components: {ParticipantListItem},
+    methods: {
+      removeParticipant(participant) {
+        let indexToRemove = this.participants.indexOf(participant);
+        this.participants.splice(indexToRemove, 1);
+      }
+    }
   };
 </script>
 
