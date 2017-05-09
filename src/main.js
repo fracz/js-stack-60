9f958e411d129f18a84da91dec777c82651ba6f8
Wojciech Frącz <fraczwojciech@gmail.com>
2017-05-09 08:32:48 +0200

    Step 1: Simple build ES6 with Webpack

---
+++ src/main.js
@@ -0,0 +1,7 @@
+class Cat {
+  constructor() {
+    alert("Meow!");
+  }
+}
+
+new Cat();
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "app.js",
+    publicPath: "/dist"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
