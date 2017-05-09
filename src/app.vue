8c8d7ef7aa87454edce655389377742044b9e99a
Wojciech Frącz <fraczwojciech@gmail.com>
2017-05-09 13:57:56 +0200

    Step 4: New features in application

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,43 @@
 <template>
   <div>
-    <h1>Epizody</h1>
+    <h1>{{ title }}</h1>
+    <form @submit.prevent="saveMe()">
+      <fieldset>
+        <legend>Zarejestruj się tutaj</legend>
+        <div>
+          <label>Imię i nazwisko</label>
+          <input type="text" v-model="newParticipant">
+        </div>
+        <div>
+          <button>Zapisz mnie</button>
+        </div>
+      </fieldset>
+    </form>
+
+    <ol>
+      <li v-for="participant in savedParticipants">{{ participant }}</li>
+    </ol>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Epizody',
+        newParticipant: '',
+        savedParticipants: []
+      }
+    },
+    methods: {
+      saveMe() {
+        if (this.newParticipant) {
+          this.savedParticipants.push(this.newParticipant);
+          this.newParticipant = '';
+        } else {
+          alert('You need to give us a name!');
+        }
+      }
+    }
+  };
+</script>
